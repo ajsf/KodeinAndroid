9a4ae4696e8e7c03c504f4901bacda692ad3fdeb
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-01-10 20:46:33 -1000

    Create classes and layouts for PhotosListActivity and PhotosAdapter.

---
+++ app/src/main/java/co/ajsf/kodeinapp/photoslist/view/PhotosAdapter.kt
@@ -0,0 +1,58 @@
+package co.ajsf.kodeinapp.photoslist.view
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.ajsf.kodeinapp.R
+import co.ajsf.kodeinapp.common.domain.model.Photo
+import kotlinx.android.synthetic.main.activity_photo_list.view.*
+import kotlinx.android.synthetic.main.item_photo.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class PhotosAdapter : RecyclerView.Adapter<PhotosAdapter.ViewHolder>() {
+
+    private val photos: MutableList<Photo> = arrayListOf()
+    var onItemClick: ((String) -> Unit)? = null
+
+    fun addPics(photos: List<Photo>): Unit {
+        this.photos.clear()
+        this.photos.addAll(photos)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_photo, parent, false)
+        return ViewHolder(view, onItemClick)
+    }
+
+    override fun onBindViewHolder(holder: PhotosAdapter.ViewHolder, position: Int) {
+        holder.bind(photos[position])
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+    class ViewHolder(view: View, private val onItemClick: ((String) -> Unit)?) : RecyclerView.ViewHolder(view) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(photo: Photo) {
+            with(itemView) {
+                cell.setOnClickListener { onItemClick?.invoke(photo.id) }
+                picture.loading
+                title.text = photo.author
+                unsplashLink.text = itemView.resources.getString(R.string.unsplash)
+                val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault()).parse(photo.created_at)
+                publishedAt.text = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(date)
+                if (photo.description.isNullOrEmpty()) {
+                    hashtags.visibility = View.GONE
+                } else {
+                    hashtags.visibility = View.VISIBLE
+                    hashtags.text = photo.description
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/co/ajsf/kodeinapp/photoslist/view/PhotosListActivity.kt
@@ -0,0 +1,79 @@
+package co.ajsf.kodeinapp.photoslist.view
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.ajsf.kodeinapp.R
+import co.ajsf.kodeinapp.common.di.InjectedActivity
+import co.ajsf.kodeinapp.common.domain.model.Photo
+import co.ajsf.kodeinapp.photoslist.di.photoListActivityModule
+import co.ajsf.kodeinapp.photoslist.presenter.PhotoListPresenter
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_photo_list.*
+import org.kodein.di.Kodein
+import org.kodein.di.generic.instance
+
+class PhotosListActivity : InjectedActivity(), PhotoListPresenter.View {
+
+    private val presenter by instance<PhotoListPresenter>()
+    private val adapter = PhotosAdapter()
+
+    override fun activityModule() = Kodein.Module("photoListActivityModule") {
+        import(photoListActivityModule())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_list)
+        setSupportActionBar(toolbar)
+        setupPhotosList()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.resume(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.pause()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_photo_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.action_settings -> true
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun setupPhotosList() {
+        photoList.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        photoList.layoutManager = layoutManager
+        photoList.adapter = adapter
+        adapter.onItemClick = ::onItemClick
+    }
+
+    private fun onItemClick(id: String): Unit = presenter.onPhotoClick(id)
+
+    override fun showLoading() {
+        loading.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        loading.visibility = View.GONE
+    }
+
+    override fun renderPhotos(photos: List<Photo>) {
+        adapter.addPics(photos)
+    }
+
+    override fun displayLoadingPhotosError() {
+        Snackbar.make(toolbar, R.string.loading_photo_error, Snackbar.LENGTH_SHORT).show()
+    }
+}

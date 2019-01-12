b11259e1c3be67d132c4b684a39f1920cde08183
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-01-11 17:30:25 -1000

    Define logger interface and implement AndroidLogger.

---
+++ app/src/main/java/co/ajsf/kodeinapp/logger/AndroidLogger.kt
@@ -0,0 +1,15 @@
+package co.ajsf.kodeinapp.logger
+
+import android.util.Log
+
+class AndroidLogger : Logger {
+    override fun log(tag: String, level: Logger.Level, message: String) {
+        when (level) {
+            Logger.Level.Info -> Log.i(tag, message)
+            Logger.Level.Debug -> Log.d(tag, message)
+            Logger.Level.Warning -> Log.w(tag, message)
+            Logger.Level.Error -> Log.e(tag, message)
+            Logger.Level.Verbose -> Log.v(tag, message)
+        }
+    }
+}
---
+++ app/src/main/java/co/ajsf/kodeinapp/logger/Logger.kt
@@ -0,0 +1,13 @@
+package co.ajsf.kodeinapp.logger
+
+interface Logger {
+    fun log(tag: String, level: Level = Level.Verbose, message: String)
+
+    sealed class Level {
+        object Info : Level()
+        object Debug : Level()
+        object Warning : Level()
+        object Error : Level()
+        object Verbose : Level()
+    }
+}

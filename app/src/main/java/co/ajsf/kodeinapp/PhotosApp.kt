385edafb1dc6e35b63afe801f3809e9466aec4e8
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-01-11 17:29:12 -1000

    Update build.gradle, AndroidManifest .gitignore and App class.

--- app/src/main/java/co/ajsf/kodeinapp/PhotosApp.kt
+++ app/src/main/java/co/ajsf/kodeinapp/PhotosApp.kt
@@ -1,13 +1,17 @@
 package co.ajsf.kodeinapp
 
 import android.app.Application
+import androidx.annotation.VisibleForTesting
+import co.ajsf.kodeinapp.common.di.appModule
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 
 class PhotosApp : Application(), KodeinAware {
 
-    override val kodein: Kodein = Kodein {
+    @VisibleForTesting
+    var overrideBindings: Kodein.MainBuilder.() -> Unit = {}
 
+    override val kodein: Kodein = Kodein.lazy {
+        import(appModule(applicationContext))
     }
-
 }
